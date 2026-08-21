7c240ad39fddbe36ca07ff5ee7c333e73d57e1e9
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-08-21 12:56:45 +0545

    add interactive banking menu

--- BankAccountSystem.java
+++ BankAccountSystem.java
@@ -35,24 +35,33 @@ class Account{
 
 public class BankAccountSystem {
   public static void main(String[] args){
+    
+    boolean isTrue = true;    //to run the while loop
 
     Account account1 = new Account("Bishesh", 1000);
     Account account2 = new Account("Bishesh2", 90000);
 
     Scanner scanner = new Scanner(System.in);
-    
-      System.out.println("Users are: ");
-      System.out.println("1. Bishesh ");
-      System.out.println("2. Bishesh2 ");
 
+    while(isTrue){
+      System.out.println("Enter (1-3) ");
+
+      System.out.println("1. View bishesh's details");
+      System.out.println("2. View bishesh2's details ");
+      System.out.println("3. Exit");
+      
+      System.out.println("Enter your choice: ");
       int choice = scanner.nextInt();
 
-    switch(choice){
-      case 1 -> account1.displayDetails();
-      case 2 -> account2.displayDetails();
-      default -> System.out.println("Wrong input");
+      switch(choice){
+        case 1 -> account1.displayDetails();
+        case 2 -> account2.displayDetails();
+        case 3 -> isTrue = false;
+        default -> System.out.println("Wrong input");
+      }
     }
 
+    System.out.println("Thank you have a nice day");
     scanner.close();
 
   }
